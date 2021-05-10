7be663542127a335df87d4c4f6729941c776c4a1
confar <tentigers@mail.ru>
2021-05-10 17:36:36 +0300

    add docs

--- course_1_algorithms/dynamic/longest_increasing_subsequence.py
+++ course_1_algorithms/dynamic/longest_increasing_subsequence.py
@@ -1,4 +1,3 @@
-import io
 from collections import deque
 
 
@@ -16,13 +15,28 @@ def restore_sequence_from_index_list(num, idx_list, orig_list):
 
 def longest_increasing_subsequence(lst):
     len_lst = len(lst)
-    d = [1] * len_lst
+    subsequence_lengths = [1] * len_lst
     for i, value in enumerate(lst):
         for j in range(i):
-            if lst[j] < value and d[j] + 1 > d[i]:
-                d[i] = d[j] + 1
-    ans = max(d)
-    return ans, d
+            if lst[j] < value and subsequence_lengths[j] + 1 > subsequence_lengths[i]:
+                subsequence_lengths[i] = subsequence_lengths[j] + 1
+    ans = max(subsequence_lengths) if len_lst else 0
+    return ans, subsequence_lengths
+
+
+def longest_non_decreasing_subsequence(str_buffer):
+    next(str_buffer)
+    lst = [int(i) for i in next(str_buffer).split(' ')]
+    len_lst = len(lst)
+    subsequence_lengths = [1] * len_lst
+    for j in range(1, len_lst):
+        for i in range(j+1):
+            if lst[i] <= lst[j] and subsequence_lengths[j] + 1 > subsequence_lengths[i]:
+                subsequence_lengths[i] = subsequence_lengths[j] + 1
+    ans = max(subsequence_lengths) if len_lst else 0
+    print(ans)
+    print(subsequence_lengths)
+    return ans, subsequence_lengths
 
 
 def longest_non_decreasing_subsequence_by_modulo(lst):
@@ -36,21 +50,15 @@ def longest_non_decreasing_subsequence_by_modulo(lst):
     return ans
 
 
-def test():
-    assert longest_increasing_subsequence(
-        [7, 2, 1, 3, 8, 4, 9, 1, 2, 6, 5, 9, 3, 8, 1]) == (5, [1, 1, 1, 2, 3, 3, 4, 1, 2, 4, 4, 5, 3, 5, 1])
-    assert restore_sequence_from_index_list(5, [1, 1, 1, 2, 3, 3, 4, 1, 2, 4, 4, 5, 3, 5, 1],
-                                               [7, 2, 1, 3, 8, 4, 9, 1, 2, 6, 5, 9, 3, 8, 1]) == [1, 3, 4, 5, 8]
-
-
-tst1 = io.StringIO('4\n3 6 7 12')
-
-
 def main():
-    test()
-    next(tst1)
-    arr = list(map(int, next(tst1).split()))
+    arr = [3, 6, 7, 12]
     assert longest_non_decreasing_subsequence_by_modulo(arr) == 3
+    assert longest_increasing_subsequence([]) == (0, [])
+    assert longest_increasing_subsequence(arr) == (4, [1, 2, 3, 4])
+    assert (longest_increasing_subsequence([7, 2, 1, 3, 8, 4, 9, 1, 2, 6, 5, 9, 3, 8, 1]) == 
+            (5, [1, 1, 1, 2, 3, 3, 4, 1, 2, 4, 4, 5, 3, 5, 1]))
+    assert restore_sequence_from_index_list(5, [1, 1, 1, 2, 3, 3, 4, 1, 2, 4, 4, 5, 3, 5, 1],
+                                            [7, 2, 1, 3, 8, 4, 9, 1, 2, 6, 5, 9, 3, 8, 1]) == [1, 3, 4, 5, 8]
 
 
 if __name__ == '__main__':
--- course_1_algorithms/dynamic/memoize_fib.py
+++ course_1_algorithms/dynamic/memoize_fib.py
@@ -1,5 +1,3 @@
-import time
-
 
 def memo(func):
     cache = {}
@@ -17,14 +15,47 @@ def memo(func):
 
 @memo
 def fib_recursive(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
+    if n <= 1:
+        return n
     return fib_recursive(n - 1) + fib_recursive(n - 2)
+
+
+def fib_recursion(n):
+    if n <= 1:
+        return n
+    return fib_recursion(n - 1) + fib_recursion(n - 2)
+
+
+def fib_recursive_top_down(n, dp_array=None):
+    if dp_array is None:
+        dp_array = [None for _ in range(n+1)]
+    if dp_array[n] is None:
+        if n <= 1:
+            dp_array[n] = n
+        else:
+            dp_array[n] = fib_recursive_top_down(n - 1, dp_array) + fib_recursive_top_down(n - 2, dp_array)
+    return dp_array[n]
+
+
+def fib_iterative_bottom_up(n):
+    dp_array = [-1 for _ in range(n+1)]
+    dp_array[0] = 0
+    dp_array[1] = 1
+    for i in range(2, n+1):
+        dp_array[i] = dp_array[i-1] + dp_array[i-2]
+    return dp_array[n]
 
 
 if __name__ == '__main__':
     assert fib_recursive(4) == 3
     assert fib_recursive(5) == 5
     assert fib_recursive(6) == 8
+    assert fib_recursive(40) == 102334155
+    assert fib_recursive_top_down(4) == 3
+    assert fib_recursive_top_down(5) == 5
+    assert fib_recursive_top_down(6) == 8
+    assert fib_recursive_top_down(40) == 102334155
+    assert fib_iterative_bottom_up(4) == 3
+    assert fib_iterative_bottom_up(5) == 5
+    assert fib_iterative_bottom_up(6) == 8
+    assert fib_iterative_bottom_up(40) == 102334155
